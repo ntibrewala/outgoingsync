6d545b6b9806681fab007bd26696747a435e535f
nikhil <administrator@RP.COM>
2026-07-02 18:24:26 +0530

    Initial commit with Outgoing Sync and Manual Recon

---
+++ OutgoingPaymentSync.vb
@@ -0,0 +1,254 @@
+﻿Imports Sap.Data.Hana
+Imports SAPbobsCOM
+Imports Bhavya.SAP.Core
+
+Module OutgoingPaymentSync
+
+    Dim sapSchema As String = "BHAVYA_POLYFILMS_01"
+    Dim DBSBankGLAccount As String = "230002"
+
+    Dim oCompany As Company
+
+    '============================================================
+    Sub Main()
+
+        Dim runId As String = Guid.NewGuid().ToString()
+
+        Logger.StartSession()
+        Logger.Log("========== [START] RunId=" & runId & " ==========")
+
+        Try
+            '=========================
+            ' HANA CONNECTION
+            '=========================
+            Using conn = HanaConnectionManager.GetConnection(sapSchema, HanaConnectionManager.HanaConnectionMode.ReadWriteMode)
+                conn.Open()
+                Logger.Log("[" & runId & "] HANA Connected")
+
+                '-------------------------
+                ' Update Bank Status
+                '-------------------------
+                If HasStatusUpdates(conn) Then
+                    Logger.Log("[" & runId & "] Updating bank payment statuses")
+
+                    Using cmd As New HanaCommand(
+                        "CALL ""DBS_BANK"".""BHV_UPDATE_PAYMENT_STATUS""()", conn)
+                        cmd.ExecuteNonQuery()
+                    End Using
+
+                    Logger.Log("[" & runId & "] Bank status updated")
+                End If
+
+                '-------------------------
+                ' Check completed payments
+                '-------------------------
+                If Not HasCompletedPayments(conn) Then
+                    Logger.Log("[" & runId & "] No completed payments. Exit.")
+                    Exit Sub
+                End If
+
+                '=========================
+                ' SAP CONNECT
+                '=========================
+                oCompany = SapCompanyManager.Connect(sapSchema)
+                Logger.Log("[" & runId & "] SAP Connected")
+
+                ProcessCompletedPayments(conn, runId)
+
+            End Using
+
+        Catch ex As Exception
+            Logger.Log("[" & runId & "] FATAL: " & ex.ToString())
+
+        Finally
+            SapCompanyManager.Disconnect(oCompany)
+            Logger.Log("========== [END] RunId=" & runId & " ==========")
+            Logger.EndSession()
+        End Try
+
+    End Sub
+
+    '============================================================
+    Function HasStatusUpdates(conn As HanaConnection) As Boolean
+        Using cmd As New HanaCommand(
+            "SELECT 1 FROM ""DBS_BANK"".""PENDING_PAYMENTS"" WHERE ""BankStatus""='PENDING' LIMIT 1", conn)
+            Return cmd.ExecuteScalar() IsNot Nothing
+        End Using
+    End Function
+
+    Function HasCompletedPayments(conn As HanaConnection) As Boolean
+        Using cmd As New HanaCommand(
+            "SELECT 1 FROM ""DBS_BANK"".""PENDING_PAYMENTS"" " &
+            "WHERE ""BankStatus""='COMPLETED' AND IFNULL(""Processed"",'N')<>'Y' LIMIT 1", conn)
+            Return cmd.ExecuteScalar() IsNot Nothing
+        End Using
+    End Function
+
+    '============================================================
+    Sub ProcessCompletedPayments(conn As HanaConnection, runId As String)
+
+        Logger.Log("[" & runId & "] Fetching completed payments")
+
+        Using cmd As New HanaCommand(
+        "CALL ""DBS_BANK"".""BHV_GET_COMPLETED_PAYMENTS""()", conn)
+
+            Using reader = cmd.ExecuteReader()
+
+                While reader.Read()
+
+                    Dim id As String = reader("Id").ToString()
+                    Dim vendor As String = reader("VendorCode").ToString()
+                    Dim amount As Double = Double.Parse(
+                        reader("PaymentAmount").ToString().Replace(",", ""),
+                        System.Globalization.CultureInfo.InvariantCulture)
+
+                    Dim txnDate As Date = GetApprovedDate(conn, id)
+
+                    Logger.Log($"[{runId}] Processing ID={id} | Date={txnDate:yyyy-MM-dd}")
+
+                    Try
+                        Dim paymentDocEntry As Integer = CreateOutgoingPayment(vendor, amount, txnDate)
+
+                        UpdatePaymentProcessed(id, paymentDocEntry, conn)
+
+                        Logger.Log($"[{runId}] SUCCESS | ID={id} | DocEntry={paymentDocEntry}")
+
+                    Catch ex As Exception
+
+                        UpdatePaymentError(id, ex.Message, conn)
+
+                        Logger.Log($"[{runId}] ERROR | ID={id} | {ex.Message}")
+
+                    End Try
+
+                End While
+
+            End Using
+        End Using
+
+    End Sub
+
+    '============================================================
+    Function GetApprovedDate(conn As HanaConnection, id As String) As Date
+
+        Using cmd As New HanaCommand(
+        "SELECT ""ApprovedAt"" FROM ""DBS_BANK"".""PENDING_PAYMENTS"" WHERE ""Id""=?", conn)
+
+            cmd.Parameters.AddWithValue("p1", id)
+
+            Dim result = cmd.ExecuteScalar()
+
+            If result IsNot Nothing Then
+                Return Convert.ToDateTime(result).Date
+            End If
+
+        End Using
+
+        Return DateTime.Today
+
+    End Function
+
+    '============================================================
+    Function CreateOutgoingPayment(vendor As String, amount As Double, txnDate As Date) As Integer
+
+        Dim oRS As Recordset = oCompany.GetBusinessObject(BoObjectTypes.BoRecordset)
+        oRS.DoQuery($"SELECT ""CardType"" FROM ""OCRD"" WHERE ""CardCode""='{vendor}'")
+
+        Dim cardType As String = If(Not oRS.EoF, oRS.Fields.Item("CardType").Value.ToString(), "")
+
+        If cardType = "S" OrElse cardType = "C" Then
+
+            Dim oPayment As Payments = oCompany.GetBusinessObject(BoObjectTypes.oVendorPayments)
+
+            oPayment.CardCode = vendor
+            oPayment.DocDate = txnDate
+            oPayment.DocType = If(cardType = "S", BoRcptTypes.rSupplier, BoRcptTypes.rCustomer)
+            oPayment.TransferAccount = DBSBankGLAccount
+            oPayment.TransferSum = amount
+            oPayment.TransferDate = txnDate
+            oPayment.Remarks = "DBS Payment - " & vendor
+
+            If oPayment.Add() <> 0 Then ThrowSapError()
+
+        Else
+
+            Dim controlAcct As String = "210001"
+
+            oRS.DoQuery($"SELECT ""DebPayAcct"" FROM ""OCRD"" WHERE ""CardCode""='{vendor}'")
+            If Not oRS.EoF Then controlAcct = oRS.Fields.Item("DebPayAcct").Value.ToString()
+
+            Dim oJE As JournalEntries = oCompany.GetBusinessObject(BoObjectTypes.oJournalEntries)
+
+            oJE.ReferenceDate = txnDate
+            oJE.DueDate = txnDate
+            oJE.Memo = "DBS Bank Payment - " & vendor
+
+            oJE.Lines.AccountCode = controlAcct
+            oJE.Lines.ShortName = vendor
+            oJE.Lines.Debit = amount
+            oJE.Lines.Add()
+
+            oJE.Lines.AccountCode = DBSBankGLAccount
+            oJE.Lines.ShortName = DBSBankGLAccount
+            oJE.Lines.Credit = amount
+
+            If oJE.Add() <> 0 Then ThrowSapError()
+
+        End If
+
+        Dim docEntry As String = ""
+        oCompany.GetNewObjectCode(docEntry)
+
+        Return Convert.ToInt32(docEntry)
+
+    End Function
+
+    '============================================================
+    Sub ThrowSapError()
+        Dim errCode As Integer = 0
+        Dim errMsg As String = ""
+        oCompany.GetLastError(errCode, errMsg)
+        Throw New Exception(errCode & " - " & errMsg)
+    End Sub
+
+    '============================================================
+    Sub UpdatePaymentProcessed(id As String, docEntry As Integer, conn As HanaConnection)
+
+        Using cmd As New HanaCommand(
+            "CALL ""DBS_BANK"".""BHV_MARK_PAYMENT_PROCESSED""(?,?)", conn)
+
+            cmd.Parameters.AddWithValue("p_id", id)
+            cmd.Parameters.AddWithValue("p_docnum", docEntry)
+            cmd.ExecuteNonQuery()
+
+        End Using
+
+        ' 🔥 Clear error
+        Using cmd2 As New HanaCommand(
+            "UPDATE ""DBS_BANK"".""PENDING_PAYMENTS"" SET ""ErrorDescription""='' WHERE ""Id""=?", conn)
+
+            cmd2.Parameters.AddWithValue("p_id", id)
+            cmd2.ExecuteNonQuery()
+
+        End Using
+
+    End Sub
+
+    Sub UpdatePaymentError(id As String, errorMsg As String, conn As HanaConnection)
+
+        Dim safeError = If(errorMsg.Length > 500, errorMsg.Substring(0, 500), errorMsg)
+
+        Using cmd As New HanaCommand(
+            "UPDATE ""DBS_BANK"".""PENDING_PAYMENTS"" SET ""ErrorDescription""=? WHERE ""Id""=?", conn)
+
+            cmd.Parameters.AddWithValue("p_error", safeError)
+            cmd.Parameters.AddWithValue("p_id", id)
+            cmd.ExecuteNonQuery()
+
+        End Using
+
+    End Sub
+    Public Sub RunOutgoing()
+        Main()
+    End Sub
+End Module
---
+++ manualrecocom.vb
@@ -0,0 +1,258 @@
+﻿Imports Sap.Data.Hana
+Imports SAPbobsCOM
+Imports Newtonsoft.Json.Linq
+Imports Bhavya.SAP.Core
+
+Module manualrecocom
+
+    Dim sapSchema As String = "BHAVYA_POLYFILMS_01"
+    Dim controlAccount As String = "210001"
+    Dim DBSBankGLAccount As String = "230002"
+
+    Dim oCompany As SAPbobsCOM.Company
+
+    Sub Main()
+
+        Dim runId As String = Guid.NewGuid().ToString()
+
+        Logger.StartSession()
+        Logger.Log("========== [ManualReco START] RunId=" & runId & " ==========")
+
+        Try
+            Dim hasRecords As Boolean = False
+
+            Using roConn = HanaConnectionManager.GetConnection(sapSchema, HanaConnectionManager.HanaConnectionMode.ReadOnlyMode)
+                roConn.Open()
+                Logger.Log("[" & runId & "] Connected to HANA (ReadOnly)")
+
+                hasRecords = HasUnreconciledPayments(roConn)
+                Logger.Log("[" & runId & "] HasUnreconciledPayments = " & hasRecords)
+            End Using
+
+            If Not hasRecords Then
+                Logger.Log("[" & runId & "] No records found. Exiting.")
+                Exit Sub
+            End If
+
+            Logger.Log("[" & runId & "] Connecting to SAP DI API...")
+            oCompany = SapCompanyManager.Connect(sapSchema)
+            Logger.Log("[" & runId & "] SAP Connected")
+
+            Using conn = HanaConnectionManager.GetConnection(sapSchema, HanaConnectionManager.HanaConnectionMode.ReadWriteMode)
+                conn.Open()
+                Logger.Log("[" & runId & "] Connected to HANA (ReadWrite)")
+
+                ProcessUnreconciledPayments(conn, runId)
+            End Using
+
+        Catch ex As Exception
+            Logger.Log("[" & runId & "] ERROR: FATAL ERROR: " & ex.ToString())
+        Finally
+            Logger.Log("[" & runId & "] Disconnecting SAP...")
+            SapCompanyManager.Disconnect(oCompany)
+
+            Logger.Log("========== [ManualReco END] RunId=" & runId & " ==========")
+            Logger.EndSession()
+        End Try
+
+    End Sub
+
+    Sub ProcessUnreconciledPayments(conn As HanaConnection, runId As String)
+
+        Logger.Log("[" & runId & "] Fetching pending payments...")
+
+        Dim sql As String =
+        "SELECT P.""Id"", P.""VendorCode"", P.""DocNum"" AS PaymentDocEntry, " &
+        "TO_NVARCHAR(P.""InvoicesJson"") AS InvoicesJson, " &
+        "IFNULL(P.""Reconciled"",'N') AS ReconState " &
+        "FROM ""DBS_BANK"".""PENDING_PAYMENTS"" P " &
+        "WHERE P.""Processed"" = 'Y' " &
+        "AND IFNULL(P.""Reconciled"",'N') IN ('N','1','2') " &
+        "AND P.""InvoicesJson"" IS NOT NULL " &
+        "AND TO_NVARCHAR(P.""InvoicesJson"") <> '[]'"
+
+        Using cmd As New HanaCommand(sql, conn)
+            Using reader = cmd.ExecuteReader()
+
+                While reader.Read()
+
+                    Dim id = reader("Id").ToString()
+                    Dim vendor = reader("VendorCode").ToString()
+                    Dim paymentDocEntry = Convert.ToInt32(reader("PaymentDocEntry"))
+                    Dim json = reader("InvoicesJson").ToString()
+                    Dim state = reader("ReconState").ToString()
+
+                    Logger.Log("[" & runId & "] START Payment | ID=" & id & " | Vendor=" & vendor & " | DocEntry=" & paymentDocEntry & " | State=" & state)
+                    Logger.Log("[" & runId & "] JSON = " & json)
+
+                    Try
+                        ReconcilePaymentUsingDIAPI(vendor, paymentDocEntry, json, conn, runId)
+
+                        UpdateReconciled(id, "Y", conn)
+                        Logger.Log("[" & runId & "] SUCCESS Payment ID=" & id)
+
+                    Catch ex As Exception
+
+                        Dim nextState As String = "1"
+                        If state = "1" Then nextState = "2"
+                        If state = "2" Then nextState = "F"
+
+                        UpdateReconciled(id, nextState, conn)
+                        Logger.Log("[" & runId & "] Updated Reconciled state to " & nextState & " for ID=" & id)
+                        Logger.Log("[" & runId & "] ERROR: FAILED Payment ID=" & id & " | NextState=" & nextState & " | Error=" & ex.ToString())
+
+                    End Try
+
+                    Logger.Log("[" & runId & "] END Payment ID=" & id)
+
+                End While
+
+            End Using
+        End Using
+
+    End Sub
+
+    Sub ReconcilePaymentUsingDIAPI(vendor As String, paymentDocEntry As Integer, invoiceJson As String, conn As HanaConnection, runId As String)
+
+        Logger.Log("[" & runId & "] Reconciliation START | PaymentDocEntry=" & paymentDocEntry)
+
+        Dim arr = JArray.Parse(invoiceJson)
+
+        Dim paymentTransId As Integer = 0
+        Dim paymentLineId As Integer = 0
+
+        GetPaymentJEInfo(paymentDocEntry, paymentTransId, paymentLineId, conn, runId)
+
+        Logger.Log("[" & runId & "] Payment JE | TransId=" & paymentTransId & " | LineId=" & paymentLineId)
+
+        Dim oCmpSrv = oCompany.GetCompanyService()
+        Dim oReconSrv = oCmpSrv.GetBusinessService(ServiceTypes.InternalReconciliationsService)
+
+        Dim oRecon = oReconSrv.GetDataInterface(
+        InternalReconciliationsServiceDataInterfaces.irsInternalReconciliationOpenTrans)
+
+        oRecon.ReconDate = DateTime.Now
+        oRecon.CardOrAccount = CardOrAccountEnum.coaCard
+
+        Dim total As Double = 0
+
+        For Each item In arr
+
+            Dim invTransId = CInt(item("InvoiceNumber"))
+
+            Dim docEntry As Integer = 0
+            Dim lineId As Integer = 0
+            Dim amount As Double = 0
+
+            GetInvoiceJEInfo(invTransId, docEntry, lineId, amount, conn, runId)
+
+            Logger.Log("[" & runId & "] Invoice | TransId=" & invTransId & " | LineId=" & lineId & " | Amount=" & amount)
+
+            Dim row = oRecon.InternalReconciliationOpenTransRows.Add()
+            row.Selected = BoYesNoEnum.tYES
+            row.TransId = invTransId
+            row.TransRowId = lineId
+            row.ReconcileAmount = amount
+
+            total += amount
+
+        Next
+
+        Logger.Log("[" & runId & "] Total Invoice Amount = " & total)
+
+        Dim payRow = oRecon.InternalReconciliationOpenTransRows.Add()
+        payRow.Selected = BoYesNoEnum.tYES
+        payRow.TransId = paymentTransId
+        payRow.TransRowId = paymentLineId
+        payRow.ReconcileAmount = total
+
+        Logger.Log("[" & runId & "] Posting reconciliation...")
+
+        oReconSrv.Add(oRecon)
+
+        Logger.Log("[" & runId & "] Reconciliation DONE | PaymentDocEntry=" & paymentDocEntry)
+
+    End Sub
+
+    Sub UpdateReconciled(id As String, state As String, conn As HanaConnection)
+
+        Using cmd As New HanaCommand(
+        "UPDATE ""DBS_BANK"".""PENDING_PAYMENTS"" SET ""Reconciled""=? WHERE ""Id""=?", conn)
+
+            cmd.Parameters.AddWithValue("p_state", state)
+            cmd.Parameters.AddWithValue("p_id", id)
+            cmd.ExecuteNonQuery()
+
+        End Using
+
+    End Sub
+
+    Function HasUnreconciledPayments(conn As HanaConnection) As Boolean
+
+        Using cmd As New HanaCommand(
+        "SELECT 1 FROM ""DBS_BANK"".""PENDING_PAYMENTS"" " &
+        "WHERE ""Processed""='Y' AND IFNULL(""Reconciled"",'N') IN ('N','1','2') LIMIT 1", conn)
+
+            Return cmd.ExecuteScalar() IsNot Nothing
+
+        End Using
+
+    End Function
+
+    Private Sub GetInvoiceJEInfo(invTransId As Integer,
+                            ByRef docEntry As Integer,
+                            ByRef lineId As Integer,
+                            ByRef actualAmount As Double,
+                            conn As HanaConnection,
+                            runId As String)
+
+        Using cmd As New HanaCommand(
+            "SELECT P.""DocEntry"", J.""Line_ID"", J.""Credit"" " &
+            "FROM """ & sapSchema & """.""OPCH"" P " &
+            "INNER JOIN """ & sapSchema & """.""JDT1"" J ON P.""TransId"" = J.""TransId"" " &
+            "WHERE P.""TransId"" = " & invTransId &
+            " AND J.""Account"" = '" & controlAccount & "' AND J.""Credit"" > 0", conn)
+
+            Using reader = cmd.ExecuteReader()
+                If reader.Read() Then
+                    docEntry = Convert.ToInt32(reader("DocEntry"))
+                    lineId = Convert.ToInt32(reader("Line_ID"))
+                    actualAmount = Convert.ToDouble(reader("Credit"))
+                Else
+                    Throw New Exception("Invoice JE not found | TransId=" & invTransId)
+                End If
+            End Using
+
+        End Using
+
+    End Sub
+
+    Private Sub GetPaymentJEInfo(paymentDocEntry As Integer,
+                            ByRef transId As Integer,
+                            ByRef lineId As Integer,
+                            conn As HanaConnection,
+                            runId As String)
+
+        Using cmd As New HanaCommand(
+            "SELECT V.""TransId"", J.""Line_ID"" " &
+            "FROM """ & sapSchema & """.""OVPM"" V " &
+            "INNER JOIN """ & sapSchema & """.""JDT1"" J ON V.""TransId"" = J.""TransId"" " &
+            "WHERE V.""DocEntry"" = " & paymentDocEntry &
+            " AND J.""Debit"" > 0 AND J.""Account"" <> '" & DBSBankGLAccount & "'", conn)
+
+            Using reader = cmd.ExecuteReader()
+                If reader.Read() Then
+                    transId = Convert.ToInt32(reader("TransId"))
+                    lineId = Convert.ToInt32(reader("Line_ID"))
+                Else
+                    Throw New Exception("Payment JE not found | DocEntry=" & paymentDocEntry)
+                End If
+            End Using
+
+        End Using
+
+    End Sub
+    Public Sub RunManualReco()
+        Main()
+    End Sub
+End Module
